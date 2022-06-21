915d268e42c7ec1f18bc7679555066059aca97d0
Vinicius Craveiro <viniciuscraveiroo@gmail.com>
2022-06-21 12:03:26 -0300

    EXC01

---
+++ ALGORITMO/L1_EXC01.java
@@ -0,0 +1,8 @@
+package ALGORITMO;
+//1) Solicitar o nome e o salário de uma pessoa. No final,
+// apresente o nome e seu novo salário como aumento de 28,5%.
+public class L1_EXC01 {
+    public static void main(String[] args) {
+
+    }
+}
